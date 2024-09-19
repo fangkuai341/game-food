81c1b6f2a6f4c940fdb70bff68dd17dd9a0102dc
LAPTOP-KHEUTB4N\lenovo <2608796981@qq.com>
2024-09-19 09:50:31 +0800

    ss

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,20 +1,51 @@
-<<template>
+<
+<template>
   <div>
-    <div>
-      <button style="margin-bottom: 30px;">开始</button>
-      <div style="font-size: 12px; color: #333;"><div>该程序未经过测试，可能会有暗病</div>
-      <div>若出现bug,大多数刷新页面可以解决</div>
-      <div>若出现无法解决的问题，请联系管理员</div>
-      <div>该游戏开源，希望多多pr,地址：https://github.com/fangkuai341/game-food</div></div>  
-      
+    <div v-if="!start">
+      <button style="margin-bottom: 30px" @click="start = true">开始</button>
+      <div style="font-size: 12px; color: #333">
+        <div>该程序未经过测试，可能会有暗病</div>
+        <div>若出现bug,大多数刷新页面可以解决</div>
+        <div>若出现无法解决的问题，请联系管理员</div>
+        <div>
+          该游戏开源，希望多多pr,地址：https://github.com/fangkuai341/game-food
+        </div>
+      </div>
     </div>
+    <div v-else>
+      <img :src="getImageUrl(foodsList[index].img)" style="width: 300px" />
+      <div style="display: flex; flex-wrap: wrap">
+        <div
+          v-for="(item, indexa) in foodsList[index].children"
+          :key="item.id"
+          style="
+            display: flex;
+            width: 50%;
+            height: 20vh;
+            justify-content: space-around;
+            margin-bottom: 8px;
+            align-items: center;
+          "
+        >
+          <img :src="getImageUrl(item.img)" alt="" style="height: 100%" />
+          <div v-if="foodsList[index].children.length !== indexa + 1">+</div>
+          <div v-else></div>
+        </div>
+      </div>
+      <button>制作</button>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { foodsList } from "./data.data";
+import { ref } from "vue";
+const start = ref(false);
+const index = Math.floor(Math.random() * 3);
 
+function getImageUrl(imageName: string) {
+  return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+}
 </script>
 
-<style >
-
-</style>
+<style></style>
--- src/view/data.data.ts
+++ src/view/data.data.ts
@@ -1,15 +1,49 @@
-const colorList=[
-    '#8E0000',
-    '#AA2655',
-    '#9726AA',
-    '#5A4085',
-    '#404393',
-    '#2B5B87',
-    '#307888',
-    '#318169',
-    '#2F7141',
-    '#506C2E',
-    '#7D7742',
-    '#A05715',
-]
-const 
+const colorList = [
+  "#8E0000",
+  "#AA2655",
+  "#9726AA",
+  "#5A4085",
+  "#404393",
+  "#2B5B87",
+  "#307888",
+  "#318169",
+  "#2F7141",
+  "#506C2E",
+  "#7D7742",
+  "#A05715",
+];
+export const foodsList = [
+  {
+    id: 1,
+    name: "仙人快乐茶",
+    img: "1-1.png",
+    children: [
+      { id: 1, name: "热浮羊奶", img: "0-1.png", sort: 1 },
+      { id: 2, name: "美梦糖浆", img: "0-2.png", sort: 2 },
+      { id: 3, name: "固态净水", img: "0-3.png", sort: 3 },
+      { id: 4, name: "一人嘉禾", img: "0-4.png", sort: 4 },
+    ],
+  },
+  {
+    id: 2,
+    name: "烈焰浓茶",
+    img: "1-2.png",
+    children: [
+      { id: 1, name: "火之翎", img: "0-5.png", sort: 1 },
+      { id: 4, name: "一人嘉禾", img: "0-4.png", sort: 2 },
+      { id: 2, name: "相位灵火", img: "0-6.png", sort: 3 },
+      { id: 3, name: "固态净水", img: "0-3.png", sort: 4 },
+    ],
+  },
+  {
+    id: 3,
+    name: "催吐剂",
+    img: "1-2.png",
+    children: [
+      { id: 1, name: "垃圾", img: "0-7.png", sort: 1 },
+      { id: 4, name: "火之翎", img: "0-5.png", sort: 2 },
+      { id: 2, name: "相位灵火", img: "0-6.png", sort: 3 },
+      { id: 3, name: "固态净水", img: "0-3.png", sort: 4 },
+    ],
+  },
+];
